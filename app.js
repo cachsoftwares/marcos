efa4ec4f2fe384a06e03e18b0d7040db74c7fdec
cachsoftwares <cachsoftwares@gmail.com>
2024-10-02 13:30:11 +0100

    Correção da Porta
    Substituição da Porta Padrão para Porta enviada pelo serviço de Hospedagem

--- app.js
+++ app.js
@@ -281,7 +281,7 @@ app.get('/info', (req, res) => {
 })
 
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log('Servidor rodando  -- localhost:' + PORT)
 })
